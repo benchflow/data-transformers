2cc2904e2dd3ee9aaf4a39f552d8f0001bc0b270
Cerfoglg <gabrielecerfoglio225@gmail.com>
2015-10-22 18:49:27 +0200

    Some fixes

--- mysqlTransformer.py
+++ mysqlTransformer.py
@@ -3,6 +3,9 @@ import sys
 from pyspark_cassandra import CassandraSparkContext
 from pyspark import SparkConf
 
+# Hard coded for now
+# This saves to a Cassandra database the values of ID and START_TIME_ from the CSV file.
+
 conf = SparkConf() \
     .setAppName("PySpark Cassandra Test") \
     .setMaster(sys.argv[1]) \
@@ -10,6 +13,7 @@ conf = SparkConf() \
 
 sc = CassandraSparkContext(conf=conf)
 
-data = sc.textFile("/Users/Gabo/PycharmProjects/Test/Camunda_dump_example_csv.csv")
+#data = sc.textFile("/Users/Gabo/PycharmProjects/Test/Camunda_dump_example_csv.csv.gz")
+data = sc.textFile(sys.argv[3])
 
-count = data.map(lambda line: line.split(",")).map(lambda a: {"id" : a[0], "name" : a[1]}).saveToCassandra("test", "info")
+count = data.map(lambda line: line.split(",")).map(lambda a: {"id" : a[0], "start_time" : a[5]}).saveToCassandra("test", "info")
