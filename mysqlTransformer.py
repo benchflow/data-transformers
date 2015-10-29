e360004b78919f63f46acc8fb32ca9d632b793f2
Cerfoglg <gabrielecerfoglio225@gmail.com>
2015-10-29 17:16:42 +0100

    Added dynamic config

--- mysqlTransformer.py
+++ mysqlTransformer.py
@@ -1,13 +1,11 @@
 import sys
+import json
 
 from pyspark_cassandra import CassandraSparkContext
 from pyspark import SparkConf
+from pyspark import SparkFiles
 
 # Takes arguments: Spark master, Cassandra host, location of the CSV file
-
-# Hard coded for now
-# This saves to a Cassandra database the values of ID and START_TIME_ from the CSV file.
-columns = ["ID_", "START_TIME_"]
 
 conf = SparkConf() \
     .setAppName("PySpark Cassandra Test") \
@@ -16,21 +14,27 @@ conf = SparkConf() \
 
 sc = CassandraSparkContext(conf=conf)
 
-#data = sc.textFile("/Users/Gabo/PycharmProjects/Test/Camunda_dump_example_csv.csv.gz")
 data = sc.textFile(sys.argv[3])
+
+confPath = SparkFiles.get("conf.json")
+with open(confPath) as f:
+    conf = json.load(f)
 
 indexes = {}
 schema = data.first().split(",")
 
 for i in range(len(schema)):
-    for c in columns:
+    for c in conf["column_mapping"]:
         if schema[i].replace('"', '') == c:
-            indexes[schema[i].replace('"', '').lower()] = i
+            indexes[schema[i].replace('"', '')] = i
+
+print(schema)
+print(indexes)
 
 def createDic(a):
     d = {}
     for i in indexes:
-        d[i] = a[indexes[i]]
+        d[conf["column_mapping"][i]] = a[indexes[i]]
     return d
 
-count = data.map(lambda line: line.split(",")).map(createDic).saveToCassandra("test", "info")
+count = data.map(lambda line: line.split(",")).map(createDic).saveToCassandra("test", conf["dest_table"])
