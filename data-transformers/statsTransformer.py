40405ce9fdebf87fb6efcd9a6f29f7886b22edd3
Cerfoglg <gabrielecerfoglio225@gmail.com>
2016-01-11 18:40:00 +0100

    Made changes to CPU percentage stats

--- data-transformers/statsTransformer.py
+++ data-transformers/statsTransformer.py
@@ -47,12 +47,11 @@ data = sc.parallelize(lines)
 def createDict(a):
     ob = json.loads(a.decode())
     d = {}
-    if (ob['precpu_stats']['cpu_usage'] != None) and ("total_usage" in ob['precpu_stats']['cpu_usage'].keys()):
+    if (ob["precpu_stats"]["cpu_usage"] != None) and ("total_usage" in ob["precpu_stats"]["cpu_usage"].keys()):
         cpu_percent = 0.0
-        cpu_delta = ob["cpu_stats"]["cpu_usage"]["total_usage"] - ob['precpu_stats']['cpu_usage']['total_usage']
-        system_delta = ob["cpu_stats"]["system_cpu_usage"] - ob['precpu_stats']['system_cpu_usage']
-        if system_delta > 0 and cpu_delta > 0:
-            cpu_percent = 100.0 * cpu_delta / system_delta * len(ob["cpu_stats"]["cpu_usage"]["percpu_usage"])
+        cpu_delta = ob["cpu_stats"]["cpu_usage"]["total_usage"] - ob["precpu_stats"]["cpu_usage"]["total_usage"]
+        system_delta = ob["cpu_stats"]["system_cpu_usage"] - ob["precpu_stats"]["system_cpu_usage"]
+        cpu_percent = (100.0 * cpu_delta) / float(system_delta * len(ob["cpu_stats"]["cpu_usage"]["percpu_usage"]))
         d["cpu_percent_usage"] = cpu_percent 
     d["environment_data_id"] = uuid.uuid1()
     d["trial_id"] = trialID
@@ -61,8 +60,8 @@ def createDict(a):
     d["read_time"] = ob["read"]
     d["cpu_total_usage"] = long(ob["cpu_stats"]["cpu_usage"]["total_usage"])
     d["cpu_percpu_usage"] = map(long, ob["cpu_stats"]["cpu_usage"]["percpu_usage"])
-    d["memory_usage"] = long(ob["memory_stats"]["usage"])
-    d["memory_max_usage"] = long(ob["memory_stats"]["max_usage"])
+    d["memory_usage"] = long(ob["memory_stats"]["usage"]/1000000)
+    d["memory_max_usage"] = long(ob["memory_stats"]["max_usage"]/1000000)
     d["cpu_throttling_data"] = ob["cpu_stats"]["throttling_data"]
     return d
 
