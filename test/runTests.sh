9c97d02d78fc20c81dabf118abbff8d6705344df
Vincenzo Ferme <fermevincenzo@gmail.com>
2016-07-08 16:44:29 +0200

    Fixes reference to sut test configuration file

--- test/runTests.sh
+++ test/runTests.sh
@@ -10,6 +10,7 @@ echo "Starting Spark tests"
 
 SPARK_MASTER=local[*]
 PYSPARK_CASSANDRA=/pyspark-cassandra-assembly-0.3.5.jar
+SUT_TEST_CONFIGURATION_FILE=/test/sut/wfms/camunda/3.5.0-3.7.0/data-transformers.configuration.yml
 CASSANDRA_HOST=cassandra
 MINIO_HOST="minio"
 MINIO_PORT="9000"
@@ -23,7 +24,7 @@ $SPARK_HOME/bin/spark-submit \
 --jars $PYSPARK_CASSANDRA \
 --driver-class-path $PYSPARK_CASSANDRA \
 --conf spark.cassandra.connection.host=$CASSANDRA_HOST \
---files $TRANSFORMERS_PATH/conf/data-transformers/camunda.data-transformers.yml \
+--files $SUT_TEST_CONFIGURATION_FILE \
 --py-files $PYSPARK_CASSANDRA,$TRANSFORMERS_PATH/commons/commons.py \
 $TRANSFORMERS_PATH/transformers/statsTransformer.py \
 '{"cassandra_keyspace":"benchflow", "minio_host": "'$MINIO_HOST'", "minio_port":"'$MINIO_PORT'", "minio_access_key":"AKIAIOSFODNN7EXAMPLE", "minio_secret_key":"wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY", "file_bucket":"runs", "file_path": "mockStats/mock", "sut_name": "'$SUT_NAME'", "trial_id": "'$TRIAL_ID'", "experiment_id": "'$EXPERIMENT_ID'", "container_id": "stats_camunda", "host_id": "'$HOST_NAME'"}'
@@ -37,7 +38,7 @@ $SPARK_HOME/bin/spark-submit \
 --jars $PYSPARK_CASSANDRA \
 --driver-class-path $PYSPARK_CASSANDRA \
 --conf spark.cassandra.connection.host=$CASSANDRA_HOST \
---files $TRANSFORMERS_PATH/conf/data-transformers/camunda.data-transformers.yml \
+--files $SUT_TEST_CONFIGURATION_FILE \
 --py-files $PYSPARK_CASSANDRA,$TRANSFORMERS_PATH/commons/commons.py \
 $TRANSFORMERS_PATH/transformers/mysqlTransformer.py \
 '{"cassandra_keyspace":"benchflow", "minio_host": "'$MINIO_HOST'", "minio_port":"'$MINIO_PORT'", "minio_access_key":"AKIAIOSFODNN7EXAMPLE", "minio_secret_key":"wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY", "file_bucket":"runs", "file_path": "mockProcessEngine", "sut_name": "'$SUT_NAME'", "trial_id": "'$TRIAL_ID'", "experiment_id": "'$EXPERIMENT_ID'", "container_id": "mysql_camunda", "host_id": "'$HOST_NAME'"}'
